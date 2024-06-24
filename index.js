28b38ad85811185eac02e486b382a7d8663ec7c1
jayjoel <166020781+jayjoel@users.noreply.github.com>
2024-06-24 22:18:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 
 // Write your code here
-const num1 = 31; // You can choose appropriate values that multiply to 62
-const num2 = 2;  // For example, 31 * 2 = 62
+const num1 = 31; 
+const num2 = 2;  
 
 const multiply = num1 * num2;
 
-const random = Math.floor(Math.random() * 100) + 1; // Generates a random integer greater than 0
+const random = Math.floor(Math.random() * 100) + 1;
 
-const num3 = 16; // Example values where the remainder of num3 % num4 is 4
+const num3 = 16; 
 const num4 = 6;
 
 const mod = num3 % num4;
 
-const max = Math.max(1, 5, 10, 20); // 20 is the highest number in this set
+const max = Math.max(1, 5, 10, 20); 
 
 module.exports = { num1, num2, multiply, random, num3, num4, mod, max };
